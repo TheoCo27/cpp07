2e8c43e3d2ed19deb00511c1f2355e67b237a91f
TheoCo27 <theocohen27@gmail.com>
2025-06-29 00:17:34 +0200

    ex01 in progress

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: theog <theog@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/29 00:00:56 by theog             #+#    #+#             */
-/*   Updated: 2025/06/29 00:11:20 by theog            ###   ########.fr       */
+/*   Updated: 2025/06/29 00:12:21 by theog            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,34 @@
 
 int main()
 {
-    int a = 1;
-    int b = 2;
+    int a = 2;
+    int b = 3;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    return 0;
+    // int a = 1;
+    // int b = 2;
 
-    std::cout << "min between " << a << " and " << b << " is " << min(a, b) << std::endl;
-    std::cout << "max between " << a << " and " << b << " is " << max(a, b) << std::endl;
-    swap(a, b);
-    std::cout << "after swap " << "max between " << a << " and " << b << " is " << max(a, b) << std::endl;
+    // std::cout << "min between " << a << " and " << b << " is " << min(a, b) << std::endl;
+    // std::cout << "max between " << a << " and " << b << " is " << max(a, b) << std::endl;
+    // swap(a, b);
+    // std::cout << "after swap " << "max between " << a << " and " << b << " is " << max(a, b) << std::endl;
 
-    double y = 42.0;
-    double x = 44.0;
+    // double y = 42.0;
+    // double x = 44.0;
 
-    std::cout << "min between " << y << " and " << x << " is " << min(y, x) << std::endl;
-    std::cout << "max between " << y << " and " << x << " is " << max(y, x) << std::endl;
-    swap(y, x);
-    std::cout << "after swap " << "max between " << y << " and " << x << " is " << max(y, x) << std::endl;
+    // std::cout << "min between " << y << " and " << x << " is " << min(y, x) << std::endl;
+    // std::cout << "max between " << y << " and " << x << " is " << max(y, x) << std::endl;
+    // swap(y, x);
+    // std::cout << "after swap " << "max between " << y << " and " << x << " is " << max(y, x) << std::endl;
 
 
 }
