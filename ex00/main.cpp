e15ce8fe5cf2221532f39a371dda74c56c1936d3
TheoCo27 <theocohen27@gmail.com>
2025-06-30 19:41:14 +0200

    light correction done

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: theog <theog@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/29 00:00:56 by theog             #+#    #+#             */
-/*   Updated: 2025/06/29 00:12:21 by theog            ###   ########.fr       */
+/*   Updated: 2025/06/30 19:40:51 by theog            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ int main()
 {
     int a = 2;
     int b = 3;
-    ::swap( a, b );
+    swap( a, b );
     std::cout << "a = " << a << ", b = " << b << std::endl;
     std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
     std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
     std::string c = "chaine1";
     std::string d = "chaine2";
-    ::swap(c, d);
+    swap(c, d);
     std::cout << "c = " << c << ", d = " << d << std::endl;
     std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
     std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
@@ -45,32 +45,4 @@ int main()
     // std::cout << "after swap " << "max between " << y << " and " << x << " is " << max(y, x) << std::endl;
 
 
-}
-
-template <typename T>
-void swap(T& a, T& b)
-{
-    T tmp;
-
-    tmp = a;
-    a = b;
-    b = tmp;
-}
-
-template <typename T>
-T& min(T& a, T& b)
-{
-    if (a < b)
-        return (a);
-    else
-        return (b);
-}
-
-template <typename T>
-T& max(T& a, T& b)
-{
-    if (a > b)
-        return (a);
-    else
-        return (b);
 }
--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -6,17 +6,36 @@
 /*   By: theog <theog@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 23:57:35 by theog             #+#    #+#             */
-/*   Updated: 2025/06/29 00:05:45 by theog            ###   ########.fr       */
+/*   Updated: 2025/06/30 19:39:15 by theog            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 template <typename T>
-void swap(T& a, T& b);
+void swap(T& a, T& b)
+{
+    T tmp;
+
+    tmp = a;
+    a = b;
+    b = tmp;
+}
 
 template <typename T>
-T& min(T& a, T& b);
+T& min(T& a, T& b)
+{
+    if (a < b)
+        return (a);
+    else
+        return (b);
+}
 
 template <typename T>
-T& max(T& a, T& b);
+T& max(T& a, T& b)
+{
+    if (a > b)
+        return (a);
+    else
+        return (b);
+}
